19f18ebf484c467f1dd67a1a03b5b5bdad7fb475
Human-Gechi <kimberlydaniel363@gmail.com>
2026-03-16 02:05:37 +0100

    refractor to check anynumnotjust 1

--- Easy/maxones/maxconsecutiveones.py
+++ Easy/maxones/maxconsecutiveones.py
@@ -1,17 +1,16 @@
 #You are given a binary array nums, return the maximum number of consecutive 1's in the array.
-
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        data = 1
-        count = 0
+        n = len(nums)
         max_count = 0
-        for val in nums:
-            if val == data:
+
+        for i in range(n):
+            count = 0
+            for j in range(i, n):
+                if nums[j] == 0:
+                    break
                 count += 1
-                if count >= max_count:
-                    max_count = count
-            else:
-                    count = 0
+                max_count = max(max_count, count)
         return max_count
 
 
