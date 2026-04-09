7795aee6e77777519cc1d574f01672afa304374e
Human-Gechi <kimberlydaniel363@gmail.com>
2026-04-09 04:40:37 +0100

    twosums O(n)

--- Easy/two-sums/twosums.py
+++ Easy/two-sums/twosums.py
@@ -1,3 +1,5 @@
+# Compleixty O(n^2)
+## My solution utilises a brute force approach . I am very much a begineer getting better at DSA
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
@@ -10,6 +12,19 @@ class Solution:
                     break
         return output
 
-## My solution utilises a brute force approach . I am very much a begineer getting better at DSA
 
-# Poco A poco
+# For complexity O(n)
+class solution:
+    def twosum(self, nums: List[int], target: int) -> List[int]:
+        l = 0 #left index
+        r = len(nums) - 1 #right index
+        while(l < r):
+            result = nums[l] + nums[r]
+            if result == target:
+                return [l, r]
+            elif result < target:
+                l += 1
+            else:
+                r -= 1
+        return []
+
