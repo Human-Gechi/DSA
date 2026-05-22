3030e9cdffd6599a6591ec2537408d8293e08be5
Human-Gechi <kimberlydaniel363@gmail.com>
2026-05-22 06:19:00 +0100

    another sol/minstacks

--- Medium/stacks/minstacks.py
+++ Medium/stacks/minstacks.py
@@ -21,3 +21,44 @@ class MinStack:
 
 # Time complexity O(1)
 # Space coplexity O(n)
+
+# Another solution
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_val = None
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.min_val = val
+            self.stack.append(val)
+        elif val < self.min_val:
+            encoded_val = 2 * val - self.min_val
+            self.stack.append(encoded_val)
+            self.min_val = val 
+        else:
+            self.stack.append(val)
+
+    def pop(self) -> None:
+        if not self.stack:
+            return
+        
+        top_val = self.stack.pop()
+
+        if top_val < self.min_val:
+            self.min_val = 2 * self.min_val - top_val
+        if not self.stack:
+            self.min_val = None
+
+    def top(self) -> int:
+        if not self.stack:
+            return None
+        
+        top_val = self.stack[-1]
+        if top_val < self.min_val:
+            return self.min_val
+        return top_val
+        
+    def getMin(self) -> int:
+        return self.min_val
